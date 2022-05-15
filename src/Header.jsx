683f5fb6446207bfb0cc1fb10823ebd009b75a7b
octopunkk <anais.derue@insa-rennes.fr>
2022-05-15 12:06:58 +0200

    add color picker for both players

--- src/Header.jsx
+++ src/Header.jsx
@@ -11,11 +11,17 @@ import CircleTwoToneIcon from "@mui/icons-material/CircleTwoTone";
 export let Header = (props) => {
   const [open, setOpen] = useState(false);
   const [colorPicked, setColorPicked] = useState(props.state.colorP1);
+  const [colorPicked2, setColorPicked2] = useState(props.state.colorP2);
+
   let openDialog = () => setOpen(true);
   let handleClose = () => setOpen(false);
   let handleClick = (e) => {
     setColorPicked(e.currentTarget.dataset.tag);
     props.pickColors(e.currentTarget.dataset.tag, "P1");
+  };
+  let handleClick2 = (e) => {
+    setColorPicked2(e.currentTarget.dataset.tag);
+    props.pickColors(e.currentTarget.dataset.tag, "P2");
   };
   return (
     <div className="header">
@@ -36,6 +42,9 @@ export let Header = (props) => {
         <Dialog open={open} onClose={handleClose}>
           <DialogTitle id="alert-dialog-title">{"Select colors"}</DialogTitle>
           <DialogContent>
+            Player 1 :
+            <br />
+            <br />
             {Object.values(props.colors).map((color, index) =>
               color == colorPicked ? (
                 <CircleTwoToneIcon
@@ -49,6 +58,28 @@ export let Header = (props) => {
                   sx={{ color: color, cursor: "pointer" }}
                   key={index}
                   onClick={handleClick}
+                  data-tag={color}
+                ></CircleIcon>
+              )
+            )}
+            <br />
+            <br />
+            Player 2 :
+            <br />
+            <br />
+            {Object.values(props.colors).map((color, index) =>
+              color == colorPicked2 ? (
+                <CircleTwoToneIcon
+                  fontSize="large"
+                  sx={{ color: color, cursor: "pointer" }}
+                  key={index}
+                ></CircleTwoToneIcon>
+              ) : (
+                <CircleIcon
+                  fontSize="large"
+                  sx={{ color: color, cursor: "pointer" }}
+                  key={index}
+                  onClick={handleClick2}
                   data-tag={color}
                 ></CircleIcon>
               )
